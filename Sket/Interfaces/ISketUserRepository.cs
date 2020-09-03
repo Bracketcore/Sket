09853755d28ad47a1915970468e124b9decd047a
Ehigiepaul <ehigiepaul@live.com>
2020-09-03 09:31:22 +0100

    created new interface

---
+++ Sket/Interfaces/ISketUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Bracketcore.Sket.Model;
+using Bracketcore.Sket.Responses;
+using System.Threading.Tasks;
+
+namespace Bracketcore.Sket.Interfaces
+{
+    public interface ISketUserRepository<T> : ISketBaseRepository<T> where T : SketUserModel
+    {
+        string HashPassword(string password);
+        Task<LoginResponse> Login(T user);
+        Task<T> Verify(T user);
+        Task<bool> LogOut(T user);
+        Task<string> Confirm(string email, string userId, string token);
+        void Reset();
+        void ChangePassword(string userId, string oldPassword, string newPassword, string resetToken);
+    }
+
+
+}

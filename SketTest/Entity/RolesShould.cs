51fc57fd79c15c52d401997f2a56bc5aa36b5097
ehigiepaul <ehigiepaul@yahoo.com>
2020-09-17 06:21:09 +0100

    working on test

---
+++ SketTest/Entity/RolesShould.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture.NUnit3;
+using Bracketcore.Sket.Entity;
+using NUnit.Framework;
+
+namespace TestProject1.Entity
+{
+    public class RolesShould
+    {
+        
+        [Theory, AutoData]
+        public void PropertiesIsSet(string str)
+        {
+            var roles = new SketRoleModel();
+
+            roles.Description = str;
+            roles.Name = str;
+            roles.Name = str;
+            
+        }
+    }
+}

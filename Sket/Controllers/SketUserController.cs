fb43638c030ac788e9b7673b47be611f83a5eebb
ehigiepaul <ehigiepaul@yahoo.com>
2020-09-29 15:34:18 +0100

    Update SketUserController.cs

--- Sket/Controllers/SketUserController.cs
+++ Sket/Controllers/SketUserController.cs
@@ -21,7 +21,8 @@ namespace Bracketcore.Sket.Controllers
     public abstract class SketUserController<T> : SketBaseController<T, ISketUserRepository<T>> where T : SketUserModel
     {
         private ISketUserRepository<T> _repo;
-        public ISketAuthenticationStateProvider<T> _authenticationStateProvider { get; set; }
+        private readonly ISketAccessTokenRepository<SketAccessTokenModel> _accessTokenRepository;
+        public AuthenticationStateProvider _authenticationStateProvider { get; set; }
 
         [AllowAnonymous]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -35,7 +36,8 @@ namespace Bracketcore.Sket.Controllers
             {
                 // todo auth schema check
                 // await HttpContext.SignInAsync(verify.ClaimsPrincipal);
-                await ((SketAuthenticationProvider<T>)_authenticationStateProvider).LoginUser(User, verify.Tk, HttpContext);
+                await ((SketAuthenticationStateProvider<T>) _authenticationStateProvider).LoginUser(User, verify.Tk,
+                    HttpContext);
                 return Ok(verify);
             }
             else
@@ -55,25 +57,53 @@ namespace Bracketcore.Sket.Controllers
             return base.Create(doc);
         }
 
-        [HttpGet("currentUser")]
+        
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost("currentUser")]
         public async Task<ActionResult> GetCurrentUser()
         {
-            await Task.Run(() => { });
-            return Ok();
+            var token = HttpContext.Request.Headers["Authorization"].ToString();
+            if (string.IsNullOrEmpty(token)) return BadRequest();
+
+            var access = await _accessTokenRepository.FindByToken(token.Replace("Bearer ", null));
+
+            if (access is null) return NotFound();
+
+            var user = await _repo.FindById(access.OwnerID.ID);
+
+            if (user is null) return NotFound();
+
+            user.Password = String.Empty;
+            return Ok(user);
         }
 
         [HttpPost("logout")]
-        public virtual async void Logout(SketUserModel user)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public virtual async Task<ActionResult> Logout(SketUserModel user)
         {
-          await  ((SketAuthenticationProvider<T>)_authenticationStateProvider).LogOutUser(HttpContext);
+            var token = HttpContext.Request.Headers["Authorization"];
+            if (!string.IsNullOrEmpty(token)) return BadRequest();
+
+            var access = await _accessTokenRepository.FindByToken(token);
+
+            if (access is null) return NotFound();
+            await _accessTokenRepository.DestroyByUserId(access.OwnerID.ID);
+
+            await ((SketAuthenticationStateProvider<T>) _authenticationStateProvider).LogOutUser(HttpContext);
+
+            return Ok();
         }
 
         protected SketUserController(ISketUserRepository<T> repo,
-            ISketAuthenticationStateProvider<T> AuthenticationStateProvider) : base(repo)
+            AuthenticationStateProvider AuthenticationStateProvider,
+            ISketAccessTokenRepository<SketAccessTokenModel> accessTokenRepository) : base(repo)
         {
             _repo = repo;
+            _accessTokenRepository = accessTokenRepository;
             _authenticationStateProvider = AuthenticationStateProvider;
         }
     }

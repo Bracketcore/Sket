cbc72c986f9f4f332943b8e2f7e8de3789e0b8b4
ehigiepaul <ehigiepaul@yahoo.com>
2020-09-29 15:34:00 +0100

    Create SketTokenHeaderHandler.cs

---
+++ Sket/Middleware/SketTokenHeaderHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Headers;
+
+namespace Bracketcore.Sket.Middleware
+{
+    public class SketTokenHeaderHandler: DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.Headers.Contains("Authorization"))
+            {
+               return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+            return await  base.SendAsync(request, cancellationToken);
+        }
+    }
+}

f4dc667f5349d80d0f075958e6e195f985199b70
ehigiepaul <ehigiepaul@yahoo.com>
2020-10-01 05:41:38 +0100

    Update SketTokenHeaderHandler.cs
    formated code

--- Sket/Middleware/SketTokenHeaderHandler.cs
+++ Sket/Middleware/SketTokenHeaderHandler.cs
@@ -6,15 +6,17 @@ using Microsoft.AspNetCore.Http.Headers;
 
 namespace Bracketcore.Sket.Middleware
 {
-    public class SketTokenHeaderHandler: DelegatingHandler
+    public class SketTokenHeaderHandler : DelegatingHandler
     {
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
         {
             if (!request.Headers.Contains("Authorization"))
             {
-               return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
             }
-            return await  base.SendAsync(request, cancellationToken);
+
+            return await base.SendAsync(request, cancellationToken);
         }
     }
 }

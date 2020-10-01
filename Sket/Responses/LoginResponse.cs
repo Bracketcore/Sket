21de99802ccdf5cb1a5cbcd49d79097070293e1e
ehigiepaul <ehigiepaul@yahoo.com>
2020-10-01 20:38:13 +0100

    refactored code

--- Sket/Responses/LoginResponse.cs
+++ Sket/Responses/LoginResponse.cs
@@ -7,11 +7,13 @@ namespace Bracketcore.Sket.Responses
     /// <summary>
     /// Get Login data response.
     /// </summary>
-    public class LoginResponse:IDisposable 
+    public class LoginResponse:IDisposable
     {
-        public string Message { get; set; }
-        public string Tk { get; set; }
-        public DateTime CreatedOn { get; set; }
+        public string Message { get; set; } = "Invalid Credentials";
+        public string Tk { get; set; } = null;
+        public DateTime CreatedOn { get; set; }  = DateTime.Now;
+        public bool IsStatueOk { get; set; } = false;
+
         // public ClaimsPrincipal ClaimsPrincipal { get; set; }
 
         protected virtual void Dispose(bool disposing)

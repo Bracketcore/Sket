d5a72370d6ecd9d90b40263411d42601f26ed2b0
ehigiepaul <ehigiepaul@yahoo.com>
2020-10-29 11:41:53 +0100

    ignored password from json

--- Sket/Entity/Built-In/SketUserModel.cs
+++ Sket/Entity/Built-In/SketUserModel.cs
@@ -1,24 +1,15 @@
-﻿using MongoDB.Entities;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using MongoDB.Entities;
 
 namespace Bracketcore.Sket.Entity
 {
     /// <summary>
-    /// Abstract model for the User model
+    ///     Abstract model for the User model
     /// </summary>
     [Name("Users")]
     public abstract class SketUserModel : SketPersistedModel
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Realm { get; set; }
-        public bool EmailVerified { get; set; }
-        public List<string> Role { get; set; }
-        public string VerificationToken { get; set; }
-        public double PhoneVerification { get; set; }
-
         public SketUserModel()
         {
             DB.Index<SketUserModel>()
@@ -27,5 +18,15 @@ namespace Bracketcore.Sket.Entity
                 .Option(o => o.Unique = true)
                 .CreateAsync();
         }
+
+        public string Username { get; set; }
+        [JsonIgnore] public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Realm { get; set; }
+        public bool EmailVerified { get; set; }
+        public List<string> Role { get; set; }
+        public string VerificationToken { get; set; }
+        public double PhoneVerification { get; set; }
     }
 }

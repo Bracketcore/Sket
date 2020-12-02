a443f407fae6da50104551e660c25ce9a342c1a6
ehigiepaul <ehigiepaul@yahoo.com>
2020-12-02 08:33:19 +0100

    created SketAppinfo

---
+++ Sket.Core/Init/SketAppInfo.cs
@@ -0,0 +1,9 @@
+namespace Sket.Core.Init
+{
+    public class SketAppInfo
+    {
+        public string AppName { get; set; }
+        public string DomainUrl { get; set; }
+        public string AppLogo { get; set; }
+    }
+}
